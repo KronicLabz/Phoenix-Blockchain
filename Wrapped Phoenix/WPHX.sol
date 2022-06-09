86934885238ea5bf9cf45baeb0b563133e7814cf
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-06-09 04:23:10 -0500

    Create WPHX.sol

---
+++ Wrapped Phoenix/WPHX.sol
@@ -0,0 +1,72 @@
+// SPDX-Lisence-Identifier: MIT
+
+/**************************************         
+*       Wrapped Phoenix WPHX          *
+* For all your in transit token needs *                      
+*       Forked by:KronicLabz          *
+**************************************/
+
+pragma solidity ^0.4.18;
+
+contract WETH9 {
+    string public name     = "Wrapped";//Wrapped Phoenix
+    string public symbol   = "WETH";//WPHX
+    uint8  public decimals = 18;
+    //Set gas limit to 21000 BEFORE deploying
+
+    event  Approval(address indexed src, address indexed guy, uint wad);
+    event  Transfer(address indexed src, address indexed dst, uint wad);
+    event  Deposit(address indexed dst, uint wad);
+    event  Withdrawal(address indexed src, uint wad);
+
+    mapping (address => uint)                       public  balanceOf;
+    mapping (address => mapping (address => uint))  public  allowance;
+ 
+    function() public payable {
+        deposit();
+    }
+    //ignore warnings for gas, adjust limit to 21000 and carry on
+    function deposit() public payable {
+        balanceOf[msg.sender] += msg.value;
+        Deposit(msg.sender, msg.value);
+    }
+    function withdraw(uint wad) public {
+        require(balanceOf[msg.sender] >= wad);
+        balanceOf[msg.sender] -= wad;
+        msg.sender.transfer(wad);
+        Withdrawal(msg.sender, wad);
+    }
+
+    function totalSupply() public view returns (uint) {
+        return this.balance;
+    }
+
+    function approve(address guy, uint wad) public returns (bool) {
+        allowance[msg.sender][guy] = wad;
+        Approval(msg.sender, guy, wad);
+        return true;
+    }
+
+    function transfer(address dst, uint wad) public returns (bool) {
+        return transferFrom(msg.sender, dst, wad);
+    }
+
+    function transferFrom(address src, address dst, uint wad)
+        public
+        returns (bool)
+    {
+        require(balanceOf[src] >= wad);
+
+        if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
+            require(allowance[src][msg.sender] >= wad);
+            allowance[src][msg.sender] -= wad;
+        }
+
+        balanceOf[src] -= wad;
+        balanceOf[dst] += wad;
+
+        Transfer(src, dst, wad);
+
+        return true;
+    }
+}
